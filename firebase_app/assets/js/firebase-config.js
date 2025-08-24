ec182d4c2af09dfa125dc31bd6bd20691965a39a
iliya amiripour <iliyaamiripour2012@gmail.com>
2025-08-24 17:25:47 +0330

    Add files via upload

---
+++ firebase_app/assets/js/firebase-config.js
@@ -0,0 +1,9 @@
+// Fill Firebase config from your console
+window.FB_CONFIG = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
